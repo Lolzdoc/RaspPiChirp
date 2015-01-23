688af1f3831d34b69d28f2956bf7511fb4c2fc16
root <root@raspberrypi.(none)>
2015-01-23 18:41:16 +0100

    mer

--- Script.py
+++ Script.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 import smbus
+import time
 
 bus = smbus.SMBus(1)    # 0 = /dev/i2c-0 (port I2C0), 1 = /dev/i2c-1 (port I2C1)
 
@@ -8,10 +9,11 @@ DEVICE_ADDRESS = 0x20      #7 bit address (will be left shifted to add the read 
 DEVICE_REG_MODE1 = 0x00
 
 #Write a single register
-bus.write_byte_data(DEVICE_ADDRESS, DEVICE_REG_MODE1, 6)
+bus.write_byte(DEVICE_ADDRESS,6)
 
 while True:
 	print bus.read_byte_data(DEVICE_ADDRESS,0)
 	print ", "
 	print bus.read_byte_data(DEVICE_ADDRESS,5)
 	print ", slut"
+	time.sleep(.03)
