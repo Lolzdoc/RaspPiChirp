5197e0a794377ff282d0a2d986136871cfa81a3d
Hans Rietz <hans.rietz@gmail.com>
2015-01-23 20:10:48 +0100

    haa

--- Script.py
+++ Script.py
@@ -16,20 +16,21 @@ time.sleep(1)
 def readValue(address,register):
 	bus.write_byte(address,register)
 	time.sleep(.1)
-	try:
-		value = bus.read_byte_data(address,2)
-		return value	
-	except IOError:
-		return "Fel i sandningen"
+	value = bus.read_byte_data(address,2)
+	return value	
+
 	
 while True:
-	print "Fukt varde:"
-	print readValue(DEVICE_ADDRESS,0)
-	print "Temperatur varde:"
-	print readValue(DEVICE_ADDRESS,5)
-	bus.write_byte(DEVICE_ADDRESS,3)
-	print "Ljus varde"
-	print readValue(DEVICE_ADDRESS,4)
-	print "slut"
-	print ""
-	time.sleep(1)
+	try:
+		print "Fukt varde:"
+		print readValue(DEVICE_ADDRESS,0)
+		print "Temperatur varde:"
+		print readValue(DEVICE_ADDRESS,5)
+		bus.write_byte(DEVICE_ADDRESS,3)
+		print "Ljus varde"
+		print readValue(DEVICE_ADDRESS,4)
+		print "slut"
+		print ""
+		time.sleep(1)
+	except IOError:
+		
